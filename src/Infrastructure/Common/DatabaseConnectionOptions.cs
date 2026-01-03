83815678f296b31793fd8a52f870a521f7270048
Дамир <shamsutdinov723@gmail.com>
2026-01-03 19:14:18 +0700

    Проведена работа с конфигурацией ASP NET Core

---
+++ src/Infrastructure/Common/DatabaseConnectionOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace Infrastructure.Common
+{
+    public sealed class DatabaseConnectionOptions
+    {
+        public required string HostName { get; set; }
+        public required string Password { get; set; }
+        public required string UserName { get; set; }
+        public required string DatabaseName { get; set; }
+        public string CreateConnectionString()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ src/Presenters/Program.cs
@@ -0,0 +1,17 @@
+﻿using Infrastructure.Common;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+DatabaseConnectionOptions? connectionOptions = builder
+    .Configuration.GetSection(nameof(DatabaseConnectionOptions))
+    .Get<DatabaseConnectionOptions>();
+
+if (connectionOptions == null)
+    throw new ArgumentException();
+
+Console.WriteLine(connectionOptions.DatabaseName);
+Console.WriteLine(connectionOptions.UserName);
+Console.WriteLine(connectionOptions.Password);
+Console.WriteLine(connectionOptions.HostName);
+
+WebApplication app = builder.Build();
