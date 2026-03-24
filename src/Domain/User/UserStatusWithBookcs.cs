4bc1e4db6854eae8bf15858007453916cf92a488
Дамир <shamsutdinov723@gmail.com>
2026-03-24 16:29:53 +0700

    Removed Book, CatalogBook, SwapBook, User. Deleted unused classes, ProjectsStorage.(11,12,13 issue)

--- src/Domain/User/UserStatusWithBookcs.cs
+++
@@ -1,7 +0,0 @@
-﻿using Domain.User;
-
-public sealed class UserStatusWithBook : UserStatus
-{
-    public UserStatusWithBook(int key, string name) : base(1, "С книгой") { }
-    public override bool CanExecuteAction() => true;
-}
--- src/Domain/User/UserStatusWithoutBook.cs
+++
@@ -1,8 +0,0 @@
-﻿using Domain.User;
-
-public sealed class UserStatusWithoutBook : UserStatus
-{
-    public UserStatusWithoutBook(int key, string name) : base(0, "Без книги") { }
-    public override bool CanExecuteAction() => false;
-
-}
